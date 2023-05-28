b7cd482ba5ca0b04daf40aaf9098d994de7237cb
GurleenSinghCanada <sin13954@sheridancollege.ca>
2023-05-27 23:19:39 -0400

    Added Gurleen singh canada

--- src/card/Card.java
+++ src/card/Card.java
@@ -1,5 +1,17 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click 
+GURLEEN SINGH CANADA
+
+
+
+
+
+
+
+
+
+
+nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package card;
